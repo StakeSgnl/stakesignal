fa7d57217efc3a52774463cff4c4bb5dab367979
StakeSgnl <stakesignal.io@outlook.com>
2026-05-05 13:37:35 +0000

    add next env.d + lib/constants

---
+++ web/src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROGRAM_ID = process.env.NEXT_PUBLIC_PROGRAM_ID || '2WNxCQG7khECic8tNgWwPgEtEkVKrXhn9b5pSdKfR35b'
+export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || 'https://api.devnet.solana.com'
+
+export const MSOL_MINT = 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'
+export const JITOSOL_MINT = 'J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn'
